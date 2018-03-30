a9c0111376bd41d86db861cdf0b7d8af3a6f8f10
arman <ag986@njit.edu>
2018-03-29 23:51:51 -0400

    Final Commit without comments

--- tests/Unit/CarsTest.php
+++ tests/Unit/CarsTest.php
@@ -19,40 +19,40 @@ class CarsTest extends TestCase
     {
         $this->assertTrue(true);
     }
-//    public function testCarInsert()
-//    {
-//        $car=new Cars();
-//        $car->make='honda';
-//        $car->model='civic';
-//        $car->year='1996';
-//        $this->assertTrue($car->save());
-//    }
-//    public function testCarYearUpdate()
-//    {
-//        $car = Cars::find(5);
-//        $car->year='2000';
-//        $this->assertTrue($car->save());
-//    }
-//    public function testDeleteCar()
-//    {
-//        $car= Cars::find(15);
-//        $this->assertTrue($car->delete());
-//    }
-//    public function testCountCar()
-//    {
-//        $car=Cars::all();
-//        $count=$car->count();
-//        $this->assertTrue($count==50);
-//    }
-//    public function testCarYear()
-//{
-//        $user=Cars::find(6);
-//        $this->assertInternalType(IsType::TYPE_INT,$user->year);
-//    }
-//    public function testModelType(){
-//        $user=Cars::find(9);
-//        $this->assertInternalType(IsType::TYPE_STRING,$user->model);
-//    }
+    public function testCarInsert()
+    {
+        $car=new Cars();
+        $car->make='honda';
+        $car->model='civic';
+        $car->year='1996';
+        $this->assertTrue($car->save());
+    }
+    public function testCarYearUpdate()
+    {
+        $car = Cars::find(5);
+        $car->year='2000';
+        $this->assertTrue($car->save());
+    }
+    public function testDeleteCar()
+    {
+        $car= Cars::find(15);
+        $this->assertTrue($car->delete());
+    }
+    public function testCountCar()
+    {
+        $car=Cars::all();
+        $count=$car->count();
+        $this->assertTrue($count==50);
+    }
+    public function testCarYear()
+{
+        $user=Cars::find(6);
+        $this->assertInternalType(IsType::TYPE_INT,$user->year);
+    }
+    public function testModelType(){
+        $user=Cars::find(9);
+        $this->assertInternalType(IsType::TYPE_STRING,$user->model);
+    }
     public function testMakeType(){
         $user=Cars::find(9);
         $this->assertInternalType(IsType::TYPE_ARRAY,$user->model);
