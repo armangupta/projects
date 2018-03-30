71e5dacd760e7463bb4c1be6b642b2d686e7724f
arman <ag986@njit.edu>
2018-03-29 21:45:11 -0400

    insert user test

---
+++ tests/Unit/CarsTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+
+class CarsTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+    public function testUserTest()
+    {
+        $user=new User();
+        $user->name='john';
+        $user->email='abcde@gmail.com';
+        $user->password='$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm';
+        $user->remember_token=str_random(10);
+
+
+        $this->assertTrue($user->save());
+    }
+}
